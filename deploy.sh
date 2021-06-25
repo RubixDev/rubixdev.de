09c042764f2c20358fd31843a66871b83fba6d98
RubixDev <silas.groh@t-online.de>
2021-06-25 20:26:48 +0200

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ git subtree push --prefix min origin deploy
 echo -e "\033[33m..done\033[0m"
 
 # Pull branch on server
-# if [ "$USER" = "silas" ]; then
-#     echo -e "\033[1;36mPulling deploy branch to rubixdev.de\033[0m"
-#     ssh contabo "cd /var/www/rubixdev.de && git pull"
-#     echo -e "\033[33m..done\033[0m"
-# fi
+if [ "$USER" = "silas" ]; then
+    echo -e "\033[1;36mPulling deploy branch on server\033[0m"
+    ssh contabo "cd /var/www/rubixdev.de && git pull"
+    echo -e "\033[33m..done\033[0m"
+fi
