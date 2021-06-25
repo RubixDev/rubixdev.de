3a040fd4b875eeeecb3bbc713479c607d3ed11a2
RubixDev <silas.groh@t-online.de>
2021-06-25 20:09:01 +0200

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Push to deploy branch
+echo -e "\033[1;36mPushing main branch min folder to deploy branch\033[0m"
+git subtree push --prefix min origin deploy
+echo -e "\033[33m..done\033[0m"
+
+# Pull branch on server
+# if [ "$USER" = "silas" ]; then
+#     echo -e "\033[1;36mPulling deploy branch to rubixdev.de\033[0m"
+#     ssh contabo "cd /var/www/rubixdev.de && git pull"
+#     echo -e "\033[33m..done\033[0m"
+# fi
