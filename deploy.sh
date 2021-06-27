1f7412d27b6531382fdebbca384b380e3f8aee86
RubixDev <silas.groh@t-online.de>
2021-06-27 16:51:14 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,10 @@
 #!/bin/bash
 
-# # Build site
-# echo -e "\033[1;36mBuilding site with parcel\033[0m"
-# npm run build
-# git add .
-# git commit -m "Build site"
-# git push origin main
-# echo -e "\033[33m..done\033[0m"
-
-# # Push to deploy branch
-# echo -e "\033[1;36mPushing main branch dist folder to deploy branch\033[0m"
-# git subtree push --prefix dist origin deploy
-# echo -e "\033[33m..done\033[0m"
-
-# # Pull branch on server
-# if [ "$USER" = "silas" ]; then
-#     echo -e "\033[1;36mPulling deploy branch on server\033[0m"
-#     ssh contabo "cd /var/www/rubixdev.de && git pull"
-#     echo -e "\033[33m..done\033[0m"
-# fi
-
-
+# Build
 npm run build
+
+# Push to deploy branch
+echo -e "\033[1;36mPushing dist folder to deploy branch\033[0m"
 git stash save
 git checkout deploy
 git stash pop
@@ -35,3 +18,10 @@ done
 git stash save
 git checkout main
 git stash pop
+echo -e "\033[33m..done\033[0m"
+
+
+# Pull branch on server
+echo -e "\033[1;36mPulling deploy branch on server\033[0m"
+ssh contabo "cd /var/www/rubixdev.de && git pull"
+echo -e "\033[33m..done\033[0m"
